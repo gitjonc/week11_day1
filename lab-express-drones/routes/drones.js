e839e7fda79ab14bfce0dd57c3a9e3868dd7b662
Diego Méndez <diegomen15@gmail.com>
2023-04-17 19:21:43 +0200

    feature: Drone getters

--- lab-express-drones/routes/drones.js
+++ lab-express-drones/routes/drones.js
@@ -5,36 +5,46 @@ const router = express.Router();
 // require the Drone model here
 
 router.get('/', async (req, res, next) => {
-  // Iteration #2: List the drones
-  // ... your code here
+  // Drone.find()
+  // .then(drones => {
+
+  // })
+  const drones = await Drone.find();
+  res.render('drones/list', { drones })
 });
 
-router.get('/drones/create', (req, res, next) => {
+router.get('/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
 });
 
-router.get('/drones/:id', (req, res, next) => {
-  // Iteration EXTRA: List one drone
-  // ... your code here
+router.get('/:id', async (req, res, next) => {
+  // const id = req.params.id;
+  const { id } = req.params;
+  // Drone.findById(id)
+  // .then(drone => {
+  //   res.render('drones/detail', { drone })
+  // })
+  const drone = await Drone.findById(id);
+  res.render('drones/detail', { drone })
 });
 
-router.post('/drones/create', (req, res, next) => {
+router.post('/create', (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get('/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post('/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
 });
 
-router.post('/drones/:id/delete', (req, res, next) => {
+router.post('/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
 });
