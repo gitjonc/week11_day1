23c176c90275130058eadf07e32081fbf2dd6e67
Jon <jon_ander_23@protonmail.com>
2023-04-18 14:17:01 +0200

    /Create done

--- lab-express-drones/routes/drones.js
+++ lab-express-drones/routes/drones.js
@@ -1,24 +1,25 @@
-const express = require('express');
-const Drone = require('../models/Drone.model');
+const express = require("express");
+const Drone = require("../models/Drone.model");
 const router = express.Router();
 
 // require the Drone model here
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   // Drone.find()
   // .then(drones => {
 
   // })
   const drones = await Drone.find();
-  res.render('drones/list', { drones })
+  res.render("drones/list", { drones });
 });
 
-router.get('/create', (req, res, next) => {
+router.get("/create", (req, res, next) => {
   // Iteration #3: Add a new drone
   // ... your code here
+  res.render("drones/create-form");
 });
 
-router.get('/:id', async (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   // const id = req.params.id;
   const { id } = req.params;
   // Drone.findById(id)
@@ -26,25 +27,29 @@ router.get('/:id', async (req, res, next) => {
   //   res.render('drones/detail', { drone })
   // })
   const drone = await Drone.findById(id);
-  res.render('drones/detail', { drone })
+  res.render("drones/detail", { drone });
 });
 
-router.post('/create', (req, res, next) => {
+router.post("/create", async (req, res, next) => {
+  console.log(req.body);
   // Iteration #3: Add a new drone
   // ... your code here
+  const { name, propellers, maxSpeed } = req.body;
+  await Drone.create({ name, propellers, maxSpeed });
+  res.redirect("/drones");
 });
 
-router.get('/:id/edit', (req, res, next) => {
+router.get("/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
 });
 
-router.post('/:id/edit', (req, res, next) => {
+router.post("/:id/edit", (req, res, next) => {
   // Iteration #4: Update the drone
   // ... your code here
 });
 
-router.post('/:id/delete', (req, res, next) => {
+router.post("/:id/delete", (req, res, next) => {
   // Iteration #5: Delete the drone
   // ... your code here
 });
